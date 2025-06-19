aa35e1ffc76c913b7b690f53c20616be4a61ff23
AmadeoMartell <medeu207@gmail.com>
2025-06-19 22:23:19 +0500

    Initial Commit

---
+++ src/main/java/com/amadeomartell/WordStreamTranslator/WordStreamTranslatorApplication.java
@@ -0,0 +1,13 @@
+package com.amadeomartell.WordStreamTranslator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WordStreamTranslatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WordStreamTranslatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/amadeomartell/WordStreamTranslator/WordStreamTranslatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.amadeomartell.WordStreamTranslator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WordStreamTranslatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
